4017bdf4979ca9ed4ca4035758f3397efca6ce33
Alex <alexemail7@126.com>
2015-10-16 18:34:28 +0800

    maven spring

---
+++ webservice/src/main/java/com/webappx/controller/WelcomeController.java
@@ -0,0 +1,7 @@
+package com.webappx.controller;
+
+/**
+ * Created by WY on 2015/10/16.
+ */
+public class WelcomeController {
+}
--- webservice/src/test/java/TestControllerTest.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-
-/**
- * Created by WY on 2015/10/14.
- */
-
-public class TestControllerTest extends AbstractJUnit4SpringContextTests {
-}
