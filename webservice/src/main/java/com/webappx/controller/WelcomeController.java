c809237cbcec864ea80e4d9f208e35e24e58a5f7
Len <tnndwc@gmail.com>
2015-10-17 22:41:11 +0800

    spring mvc is ok

--- webservice/src/main/java/com/webappx/controller/WelcomeController.java
+++ webservice/src/main/java/com/webappx/controller/WelcomeController.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,8 +17,18 @@ public class WelcomeController {
     @Value("${application.message:Hello World}")
     private String message = "Hello World";
 
-    @RequestMapping("/hi")
+    @RequestMapping("/")
+    public String hi(Map<String, Object> model) {
+        System.out.println("======hi======");
+        model.put("time", new Date() + "---abc");
+        model.put("message", this.message);
+        return "hi";
+    }
+
+
+    @RequestMapping("/hello")
     public String welcome(Map<String, Object> model) {
+        System.out.println("======hello======");
         model.put("time", new Date());
         model.put("message", this.message);
         return "hi";
--- webservice/src/main/webapp/WEB-INF/jsp/hi.jsp
+++ webservice/src/main/webapp/WEB-INF/jsp/hi.jsp
@@ -6,13 +6,7 @@
 <html lang="en">
 
 <body>
-<c:url value="/resources/text.txt" var="url"/>
-<spring:url value="/resources/text.txt" htmlEscape="true" var="springUrl" />
-Spring URL: ${springUrl} at ${time}
-<br>
-JSTL URL: ${url}
-<br>
-Message: ${message}
+${message}
 </body>
 
 </html>
